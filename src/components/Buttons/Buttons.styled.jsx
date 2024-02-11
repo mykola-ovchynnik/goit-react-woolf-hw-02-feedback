4ca7091327e9678709686036e30d09555deed7d6
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-02-11 18:11:35 +0100

    upd buttons

--- src/components/Buttons/Buttons.styled.jsx
+++ src/components/Buttons/Buttons.styled.jsx
@@ -7,25 +7,31 @@ export const ButtonListWrapper = styled.div`
 `;
 
 export const Button = styled.div`
-  width: 100px;
-  background-color: #fff;
-  border: 1px solid #d5d9d9;
-  border-radius: 8px;
-  box-shadow: rgba(213, 217, 217, 0.5) 0 2px 5px 0;
-  box-sizing: border-box;
-  color: #0f1111;
-  font-size: 16px;
-  font-weight: 600;
-  padding: 5px 15px 5px 15px;
-  text-align: center;
+  padding: 10px 40px;
+  border-radius: 50px;
+  border: 0;
+  background-color: white;
+  box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
+  letter-spacing: 1.5px;
   text-transform: capitalize;
+  font-size: 16px;
   cursor: pointer;
+  transition: all 0.5s ease;
 
   &:hover,
   &:focus {
-    background-color: #f7fafa;
-    border-color: #008296;
-    box-shadow: rgba(213, 217, 217, 0.5) 0 2px 5px 0;
-    outline: 0;
+    letter-spacing: 3px;
+    background-color: hsl(261deg 80% 48%);
+    color: hsl(0, 0%, 100%);
+    box-shadow: rgb(93 24 220) 0px 7px 29px 0px;
+  }
+
+  &:active {
+    letter-spacing: 3px;
+    background-color: hsl(261deg 80% 48%);
+    color: hsl(0, 0%, 100%);
+    box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
+    transform: translateY(10px);
+    transition: 100ms;
   }
 `;
